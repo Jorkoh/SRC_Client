1fa8de541b355f75c45200df5447dba97399f0e4
Jorkoh <kohru95@gmail.com>
2021-05-15 11:56:39 +0200

    Variables common to the game and specific to a category now behave properly

--- src/main/kotlin/ui/screens/home/FiltersSection.kt
+++ src/main/kotlin/ui/screens/home/FiltersSection.kt
@@ -8,9 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import data.local.entities.Category
 import data.local.entities.CategoryType
 import data.local.entities.RunStatus
+import data.local.entities.Variable
 import data.local.entities.VariableAndValueIds
 import persistence.database.Filters
 import ui.utils.FlowRow
@@ -45,6 +45,9 @@ private fun FiltersRow(
         verticalGap = 8.dp,
         modifier = Modifier.padding(16.dp)
     ) {
+        val gameVariables = uiState.game.categories.firstOrNull { it.type == CategoryType.PerGame }
+            ?.variables?.filter { it.categoryId == null }
+
         // Category filter TODO add level support
         val categories = uiState.game.categories.filter { it.type == CategoryType.PerGame }
         val selectedCategory = categories.firstOrNull { it.categoryId == uiState.filters.categoryId }
@@ -52,11 +55,15 @@ private fun FiltersRow(
             title = "Category",
             selectedOption = selectedCategory,
             options = categories,
-            onOptionSelected = {
+            onOptionSelected = { newCategory ->
                 onFiltersChanged(
                     uiState.filters.copy(
-                        categoryId = it?.categoryId,
-                        variablesAndValuesIds = emptyList()
+                        categoryId = newCategory?.categoryId,
+                        variablesAndValuesIds = uiState.filters.variablesAndValuesIds.toMutableList()
+                            .filter { filterVariable ->
+                                // When changing category only keep game variable filters
+                                gameVariables?.any { filterVariable.variableId == it.variableId } ?: false
+                            }
                     )
                 )
             }
@@ -72,19 +79,29 @@ private fun FiltersRow(
             onOptionSelected = { onFiltersChanged(uiState.filters.copy(runStatus = it)) }
         )
 
-        // Custom leaderboard variables filters
-        VariableFilters(selectedCategory, uiState.filters, onFiltersChanged)
+        // Custom leaderboard variables filters from selected category
+        VariablesFilters(
+            selectedCategory?.variables?.filter { it.categoryId != null },
+            uiState.filters,
+            onFiltersChanged
+        )
+        // Custom leaderboard variables filters from game TODO add level support
+        VariablesFilters(
+            gameVariables,
+            uiState.filters,
+            onFiltersChanged
+        )
     }
 }
 
 @Composable
-private fun VariableFilters(
-    selectedCategory: Category?,
+private fun VariablesFilters(
+    variables: List<Variable>?,
     filters: Filters,
     onFiltersChanged: (Filters) -> Unit
 ) {
     // Pair the variable with its possible values and the selected one (if exists) on the filter
-    val variableValuesAndSelectedList = selectedCategory?.variables?.map { variable ->
+    val variableValuesAndSelectedList = variables?.map { variable ->
         Triple(
             variable,
             variable.values,
@@ -103,11 +120,11 @@ private fun VariableFilters(
                 selectedOption = selectedValue,
                 options = values,
                 onOptionSelected = { newValue ->
+                    // Add or remove the variable-value pair from the filter
                     val newFilter = filters.variablesAndValuesIds.toMutableList().apply {
                         removeIf { it.variableId == variable.variableId }
                         newValue?.let { add(VariableAndValueIds(variable.variableId, it.valueId)) }
                     }
-
                     onFiltersChanged(filters.copy(variablesAndValuesIds = newFilter))
                 }
             )
